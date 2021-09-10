79e5d172d244c38f29c815204301251851e55bd3
KaminskiyEvgeniy <90305307+KaminskiyEvgeniy@users.noreply.github.com>
2021-09-10 14:35:02 +0700

    Update add_comment.sql

--- tables/add_comment.sql
+++ tables/add_comment.sql
@@ -2,3 +2,21 @@ COMMENT ON TABLE clients IS 'Таблица содержит основную и
 
 COMMENT ON COLUMN clients.name IS 'ФИО клиента'
 COMMENT ON COLUMN clients.date_of_birth IS 'Дата рождения клиента';
+
+-- КОММЕНТАРИЙ однострочный
+-- Сортировка ASK, DESK
+
+SELECT price, rating
+  FROM dishes
+    ORDER BY price, rating 
+
+SELECT price, rating
+  FROM dishes
+    ORDER BY 1, 2
+
+SELECT * FROM dishes
+    ORDER BY rating NULLS LAST
+
+SELECT * FROM dishes
+    ORDER BY rating NULLS FIRST
+        
