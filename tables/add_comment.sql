2eb17e303330845cd82416ca4e698be4e21a7e5a
KaminskiyEvgeniy <90305307+KaminskiyEvgeniy@users.noreply.github.com>
2021-09-10 10:30:01 +0700

    Create add_comment.sql

---
+++ tables/add_comment.sql
@@ -0,0 +1,4 @@
+COMMENT ON TABLE clients IS 'Таблица содержит основную информацию по клиентам банка';
+
+COMMENT ON COLUMN clients.name IS 'ФИО клиента'
+COMMENT ON COLUMN clients.date_of_birth IS 'Дата рождения клиента';
