1b9775bba18e0c928d86447ad087a6cf7e441cf8
KaminskiyEvgeniy <90305307+KaminskiyEvgeniy@users.noreply.github.com>
2021-09-13 13:45:53 +0700

    Update add_foreign_key.sql

--- tables/add_foreign_key.sql
+++ tables/add_foreign_key.sql
@@ -5,3 +5,15 @@ alter table "PRODUCTS" add constraint
 alter table "PRODUCTS" add constraint
 "PROD_PROTYPE_FK" foreign key ("PRODUCT_TYPE_ID") references "PRODUCT_TYPE" ("ID")
 /
+
+alter table "ACCOUNTS" add constraint
+"ACC_CL_FK" foreign key ("CLIENT_REF") references "CLIENTS" ("ID")
+/
+
+alter table "RECORDS" add constraint
+"REC_ACC_FK(ACC_REF)" foreign key ("ACC_REF") references "ACCOUNTS" ("ID")
+/
+
+alter table "PRODUCT_TYPE" add constraint
+"PRO_TYPE_TAR_FK(TARIF_REF)" foreign key ("TARIF_REF") references "TARIFS" ("ID")
+/ 
